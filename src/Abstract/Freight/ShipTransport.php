91ccf519d36d44f3c8e2a12a4d55ad759be4b8b1
Eduardo Rogerio <eduardo.rogerio@fmd.ag>
2024-09-11 23:20:35 -0300

    feat(): exercise21.

---
+++ src/Abstract/Freight/ShipTransport.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace OopExercises\Abstract\Freight;
+
+use OopExercises\Abstract\Freight\CargoTransport;
+
+class ShipTransport extends CargoTransport
+{
+
+    public function loadCargo(): string
+    {
+        // TODO: Implement loadCargo() method.
+    }
+
+    public function unloadCargo(): string
+    {
+        // TODO: Implement unloadCargo() method.
+    }
+
+    public function calculateCost(): string
+    {
+        // TODO: Implement calculateCost() method.
+    }
+}
