3b6bddc416eda6ed98de3c047b52cab34947eb1e
eduardo-rogerio <eduardorogerio@protonmail.com>
2024-09-20 23:59:18 -0300

    feat(): exercise29

--- src/Traits/Payment/Payment.php
+++ src/Traits/Payment/Payment.php
@@ -1,16 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace OopExercises\Traits\Payment;
 
 trait Payment
 {
     public function makePayment(int $amount)
     {
-        return $amount;
+        return 'Pagamento realizado: R$' . number_format($amount);
     }
 
     public function refundPayment(int $amount)
     {
-        return $amount;
+        return 'Reenbolso realizado' . number_format($amount);
     }
 }
