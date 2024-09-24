ea40af35ea4712d10108d5fae75d215f0ebe2143
eduardo-rogerio <eduardorogerio@protonmail.com>
2024-09-23 23:20:14 -0300

    feat(): exercise32

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 
 declare(strict_types=1);
 
-use OopExercises\Traits\Scorable\Basketball;
-use OopExercises\Traits\Scorable\Soccer;
+use OopExercises\Traits\Counter\Cart;
+use OopExercises\Traits\Counter\Checkout;
 
 require 'vendor/autoload.php';
 
-$soccer = new Soccer();
-$bascketBall = new Basketball();
-
-dump("marcou um gol placar: {$soccer->increaseScore(1)} a 0", "cesta de 3 pontos placar: {$bascketBall->increaseScore(3)} a 34");
+$cart1 = new Cart;
+$cart2 = new Cart;
+$checkout1 = new Checkout;
+echo "Cart instances: " . Cart::getInstanceCount() . PHP_EOL;
+echo "Checkout instances: " . Checkout::getInstanceCount() . PHP_EOL;
